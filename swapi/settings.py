70ab9d2b306231e9984aec59b51645bca1f47d30
Mary Louise Hermosa <jayzces@gmail.com>
2020-04-13 17:19:35 +0800

    Allowing database edits

--- swapi/settings.py
+++ swapi/settings.py
@@ -62,10 +62,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-if not DEBUG:
-    DATABASES['default'] =  dj_database_url.config()
 
-    DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default'] =  dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
